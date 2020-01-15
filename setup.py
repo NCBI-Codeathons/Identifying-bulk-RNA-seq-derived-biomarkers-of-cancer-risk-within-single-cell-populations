73a9d3e6cee4f8748b81d113aec435ebbcd54490
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-01-15 10:05:50 -0500

    added some folders, added a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Identifying-bulk-RNA-seq-derived-biomarkers-of-cancer-risk-within-single-cell-populations',
+    version='',
+    packages=['src', 'src.analysis', 'src.common', 'src.preprocess', 'src.visualization'],
+    url='',
+    license='',
+    author='NYGC-singlecell-hackathon-team-biomarker',
+    author_email='',
+    description=''
+)
