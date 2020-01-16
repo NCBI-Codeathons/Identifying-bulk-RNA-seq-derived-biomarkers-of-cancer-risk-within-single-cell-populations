16b6ad6ab7887e22001509792588cee45f3a9eec
Matthew Bernstein <matthewbernstein@Matthews-MacBook-Pro-2.local>
2020-01-16 15:30:53 -0500

    Some functions for slicing matrices and normalizing

---
+++ src/common/slice_matrix.py
@@ -0,0 +1,15 @@
+######################################
+#   Basic normalization functions
+######################################
+import numpy as np
+
+def keep_genes(X, genes, keep_genes):
+    gene_to_index = {
+        gene: index
+        for index, gene in enumerate(genes)
+    }
+    indices = [
+        gene_to_index[gene]
+        for gene in keep_genes
+    ]
+    return X[:,indices]
