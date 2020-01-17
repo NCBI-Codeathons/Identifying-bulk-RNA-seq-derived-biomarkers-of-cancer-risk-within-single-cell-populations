aae4f05d79e735bb4123cf220a651ad0392ac44c
davidmayhew <david.n.mayhew@gmail.com>
2020-01-17 10:44:29 -0500

    added 10X formatted data for Seurat

--- src/analysis/SINGLE_CLUSTERING.R
+++ src/analysis/SINGLE_CLUSTERING.R
@@ -2,6 +2,16 @@
 library(dplyr)
 library(Seurat)
 
+#get arguments
+args = commandArgs(trailingOnly=TRUE)
+
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  USER_SET_CLUS_RES = 0.8
+} else {
+  USER_SET_CLUS_RES = args[1]
+}
+
 #here we are opening all of the individual runs up to MARCH152019.. these are all 20 male brains
 
 tumor1 <- Read10X(data.dir = "GSM2758471", gene.column = 1)
@@ -16,11 +26,11 @@ tumor8 <- Read10X(data.dir = "GSM2758478", gene.column = 1)
 #Create objects
 
 # Initialize the Seurat object with the raw (non-normalized data).
-T1 <- CreateSeuratObject(counts = tumor1, project = "R1", min.cells = 3, min.features = 200)
+T1 <- CreateSeuratObject(counts = tumor1, project = "R1", min.cells = 3,min.features = 200)
 T2 <- CreateSeuratObject(counts = tumor2, project = "R2", min.cells = 3,min.features = 200)
 T3 <- CreateSeuratObject(counts = tumor3, project = "R3", min.cells = 3,min.features = 200)
 T4 <- CreateSeuratObject(counts = tumor4, project = "R4", min.cells = 3,min.features = 200)
-T5 <- CreateSeuratObject(counts = tumor5, project = "R5", min.cells = 3, min.features = 200)
+T5 <- CreateSeuratObject(counts = tumor5, project = "R5", min.cells = 3,min.features = 200)
 T6 <- CreateSeuratObject(counts = tumor6, project = "R6", min.cells = 3,min.features = 200)
 T7 <- CreateSeuratObject(counts = tumor7, project = "R7", min.cells = 3,min.features = 200)
 T8 <- CreateSeuratObject(counts = tumor8, project = "R8", min.cells = 3,min.features = 200)
@@ -35,68 +45,68 @@ for (i in 1:length(x = datasets)) {
   datasets[[i]] <- ScaleData(datasets[[i]], vars.to.regress = "nFeature_RNA")
   datasets[[i]] <- RunPCA(datasets[[i]], features = VariableFeatures(object = datasets[[i]]), npcs = 30)
   datasets[[i]] <- FindNeighbors(datasets[[i]], dims = 1:30)
-  datasets[[i]] <- FindClusters(datasets[[i]], dims = 1:30, resolution = 0.8)
+  datasets[[i]] <- FindClusters(datasets[[i]], dims = 1:30, resolution = USER_SET_CLUS_RES)
 }
 
 
 # find markers for every cluster compared to all remaining cells, report only the positive ones
 T1_MK_clusters <- FindAllMarkers(T1, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 T1_MK_clusters %>% group_by(cluster) %>% top_n(n = 50, wt = avg_logFC)%>% 
-  write.csv(.,file="/data/interim/MK_genes_TUMOR1.csv")
+  write.csv(.,file="../data/interim/MK_genes_TUMOR1.csv")
 
 T2_MK_clusters <- FindAllMarkers(T2, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 T2_MK_clusters %>% group_by(cluster) %>% top_n(n = 50, wt = avg_logFC)%>% 
-  write.csv(.,file="/data/interim/MK_genes_TUMOR2.csv")
+  write.csv(.,file="../data/interim/MK_genes_TUMOR2.csv")
 
 
 T3_MK_clusters <- FindAllMarkers(T3, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 T3_MK_clusters %>% group_by(cluster) %>% top_n(n = 50, wt = avg_logFC)%>% 
-  write.csv(.,file="/data/interim/MK_genes_TUMOR3.csv")
+  write.csv(.,file="../data/interim/MK_genes_TUMOR3.csv")
 
 
 T4_MK_clusters <- FindAllMarkers(T4, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 T4_MK_clusters %>% group_by(cluster) %>% top_n(n = 50, wt = avg_logFC)%>% 
-  write.csv(.,file="/data/interim/MK_genes_TUMOR4.csv")
+  write.csv(.,file="../data/interim/MK_genes_TUMOR4.csv")
 
 T5_MK_clusters <- FindAllMarkers(T5, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 T5_MK_clusters %>% group_by(cluster) %>% top_n(n = 50, wt = avg_logFC)%>% 
-  write.csv(.,file="/data/interim/MK_genes_TUMOR5.csv")
+  write.csv(.,file="../data/interim/MK_genes_TUMOR5.csv")
 
 T6_MK_clusters <- FindAllMarkers(T6, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 T6_MK_clusters %>% group_by(cluster) %>% top_n(n = 50, wt = avg_logFC)%>% 
-  write.csv(.,file="/data/interim/MK_genes_TUMOR6.csv")
+  write.csv(.,file="../data/interim/MK_genes_TUMOR6.csv")
 
 T7_MK_clusters <- FindAllMarkers(T7, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 T7_MK_clusters %>% group_by(cluster) %>% top_n(n = 50, wt = avg_logFC)%>% 
-  write.csv(.,file="/data/interim/MK_genes_TUMOR7.csv")
+  write.csv(.,file="../data/interim/MK_genes_TUMOR7.csv")
 
 T8_MK_clusters <- FindAllMarkers(T8, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 T8_MK_clusters %>% group_by(cluster) %>% top_n(n = 50, wt = avg_logFC)%>% 
-  write.csv(.,file="/data/interim/MK_genes_TUMOR8.csv")
+  write.csv(.,file="../data/interim/MK_genes_TUMOR8.csv")
 
 
 #here you do this for the meatada
 as.data.frame(T1@meta.data)%>% 
-  write.csv(.,file="/data/interim/T1_META.csv")
+  write.csv(.,file="../data/interim/T1_META.csv")
 
 as.data.frame(T2@meta.data)%>% 
-  write.csv(.,file="/data/interim/T2_META.csv")
+  write.csv(.,file="../data/interim/T2_META.csv")
 
 as.data.frame(T3@meta.data)%>% 
-  write.csv(.,file="/data/interim/T3_META.csv")
+  write.csv(.,file="../data/interim/T3_META.csv")
 
 as.data.frame(T4@meta.data)%>% 
-  write.csv(.,file="/data/interim/T4_META.csv")
+  write.csv(.,file="../data/interim/T4_META.csv")
 
 as.data.frame(T5@meta.data)%>% 
-  write.csv(.,file="/data/interim/T5_META.csv")
+  write.csv(.,file="../data/interim/T5_META.csv")
 
 as.data.frame(T6@meta.data)%>% 
-  write.csv(.,file="/data/interim/T6_META.csv")
+  write.csv(.,file="../data/interim/T6_META.csv")
 
 as.data.frame(T7@meta.data)%>% 
-  write.csv(.,file="/data/interim/T7_META.csv")
+  write.csv(.,file="../data/interim/T7_META.csv")
 
 as.data.frame(T8@meta.data)%>% 
-  write.csv(.,file="/data/interim/T8_META.csv")
+  write.csv(.,file="../data/interim/T8_META.csv")
 
--- src/analysis/SeuratBiomarkerWrapper.py
+++ src/analysis/SeuratBiomarkerWrapper.py
@@ -1,8 +1,12 @@
 import sys, getopt, subprocess
 
+from gsea_analysis import get_gsea_result
+from color_genes_on_phate import color_genes_on_phate
+
 #use Rscript to call the Seurat clustering script
-def seurat_clusters_script():
+def seurat_clusters_script(clusteringres):
     #call the Seurat command
+    
     seuratcommandcall = "/usr/local/bin/Rscript SINGLE_CLUSTERING.R"
     subprocess.call(seuratcommandcall, shell=True)
 
@@ -11,30 +15,33 @@ def main(argv):
     
     #parse command line arguments
     inputgenelist=''
+    clusteringres=''
     
     try:
-        opts, args = getopt.getopt(argv, "hx:g:",["ifile1=","ifile2="])
+        opts, args = getopt.getopt(argv, "hg:r:",["ifile1=","ifile2="])
     except getopt.GetoptError:
-        print('SeuratBiomarkerWrapper.py -g <genes>')
+        print('SeuratBiomarkerWrapper.py -g <genes> -r <resolution>')
         sys.exit(3)
         
     for opt, arg in opts:
         if opt == '-h':
-            print('SeuratBiomarkerWrapper.py -g <genes>')
+            print('SeuratBiomarkerWrapper.py -g <genes> -r <resolution')
             sys.exit()
         elif opt in ("-g", "--genes"):
             inputgenelist = arg
+        elif opt in ("-r", "--resolution"):
+            clusteringres = arg
             
     #error conditions: missing input and confliciting command line arguments
     if inputgenelist=='':
-        print("Error: Missing input")
+        print("Error: Missing biomarker gene list input")
         sys.exit(3)
         
     #split the input of gene names
     sgenes = inputgenelist.split(",")
     print(sgenes)
     
-    seurat_clusters_script()
+    seurat_clusters_script(clusteringres)
     
 if __name__== "__main__":
     main(sys.argv[1:])
